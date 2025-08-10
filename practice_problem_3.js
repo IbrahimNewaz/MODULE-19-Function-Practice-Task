7d0bb3ef4e2338b9f6970caa52696946ee29a56d
IbrahimNewaz <ibrahim.codecoffe@gmail.com>
2025-08-11 01:58:40 +0600

    Trying New Test case The user input

--- practice_problem_3.js
+++ practice_problem_3.js
@@ -5,23 +5,44 @@ and return the average of those values.
 */
 
 // 🟩🟦 User Defined Function 🟦🟩
-function make_avg(A, n)
-{
+function make_avg(A, n) {
+
+    console.log('The Size of This Array is >>> ', n);
+
     let suMofArray = 0;
     let i = 0;
-    while(i < A.length)
-    {
+    while (i < A.length) {
         suMofArray = suMofArray + A[i];
         i++;
     }
     const avgOfvalue = suMofArray / 2;
+    console.log('The sum of the array elements is..." >> ' , suMofArray);
     return avgOfvalue;
 }
 
 
 /*🟥Main Function🟥*/
 
-const AvGarray = [11, 22, 33, 44, 55];
-const sizeOFarray = AvGarray.length;
+// const AvGarray = [11, 22, 33, 44, 55];
+// const sizeOFarray = AvGarray.length;
 
-const fiNalResult = make_avg(AvGarray , sizeOFarray);
+"use strict";
+const prosyn = require("prompt-sync");
+const prompt = prosyn();
+
+let AvGarray = [];
+let input;
+
+while (true) {
+    input = parseInt(prompt("Enter array element. Type -1 to finish: "));
+    if (input === -1) {
+        break;
+    }
+    AvGarray.push(input);
+}
+const sizeOFarray = AvGarray.length;
+console.log("The Array Inputs Are ->", AvGarray.join(" "));
+
+const fiNalResult = make_avg(AvGarray, sizeOFarray);
+console.log('Average Of Those Values', fiNalResult);
+
